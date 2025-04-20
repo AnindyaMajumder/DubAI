bf36ad1db62d434a1f20a662a40a7be77a0902c0
AnindyaMajumder <anindyamajumder.54@gmail.com>
2025-04-21 00:40:15 +0600

    Depedency installation script pushed

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pip install moviepy
+pip install pydub
+pip install gtts
+pip install pytubefix
+pip install torch torchaudio
+pip install openai-whisper
+pip install demucs
+pip install google-generativeai
+pip install python-dotenv
+pip install numpy
